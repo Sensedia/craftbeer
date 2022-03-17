c5906feb7127706b70e78f18f3594bd8416d3f94
Faslala Assis <faslala.mesquita@eticasolucoes.com.br>
2022-03-17 09:47:22 -0300

    ajustes

--- src/test/java/com/beerhouse/ApplicationTests.java
+++ src/test/java/com/beerhouse/ApplicationTests.java
@@ -1,6 +1,7 @@
 package com.beerhouse;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
